822aa5c0683101120e190085abaf0254730dfdb5
Omar-242 <omar090420@gmail.com>
2023-04-01 19:55:34 +0600

    setup

---
+++ setup/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Hello Omar, ");
+
+    printf("Please enter your age:");
+
+    scanf("%d", &n);
+
+    printf("Your age is %d", n);
+}
